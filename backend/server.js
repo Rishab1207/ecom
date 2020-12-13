45a67667b51fd0e79c40d8a9f03c0f06956fe86d
Rishab <rishabsharma120720@gmail.com>
2020-12-13 22:10:35 +0530

    Basic REST API Setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const products = require("./data/products");
+
+// Initialise app variable
+const app = express();
+
+const PORT = 5000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+	res.send("Home Page");
+});
+
+app.get("/api/products", (req, res) => {
+	res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+	res.json(product);
+});
+
+app.listen(PORT, () => console.log(`Server has been started on ${PORT}`));
