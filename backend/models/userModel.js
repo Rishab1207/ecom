33d6b8eca8b38843330e111ba90fdfa9eeb1c4c5
Rishab <rishabsharma120720@gmail.com>
2020-12-18 23:15:48 +0530

    Database Models Created

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+	},
+	{
+		timeStamps: true,
+	}
+);
+
+modult.exports = user = mongoose.model("User", userSchema);
