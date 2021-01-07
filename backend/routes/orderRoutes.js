9d4244e77b31364b837be199f55a9c2be5cff7cf
Rishab <rishabsharma120720@gmail.com>
2021-01-07 23:43:14 +0530

    Update order to paid router created

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const {
 	addOrderItems,
 	getOrderById,
+	updateOrderToPaid,
 } = require("../controllers/orderController");
 const protect = require("../middleware/authMiddleware");
 
 router.route("/").post(protect, addOrderItems);
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 module.exports = router;
