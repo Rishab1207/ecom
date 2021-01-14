ffc40a50958c152cc42f1bf49c13496b3f1f105c
Rishab <rishabsharma120720@gmail.com>
2021-01-14 22:43:05 +0530

    Get user by id and update user profile by admin route created

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,8 @@ const {
 	updateUserProfile,
 	getUsers,
 	deleteUser,
+	getUserById,
+	updateUser,
 } = require("../controllers/userControllers");
 const { protect, admin } = require("../middleware/authMiddleware");
 
@@ -17,6 +19,10 @@ router
 	.get(protect, getUserProfile)
 	.put(protect, updateUserProfile);
 
-router.route("/:id").delete(protect, admin, deleteUser);
+router
+	.route("/:id")
+	.delete(protect, admin, deleteUser)
+	.get(protect, admin, getUserById)
+	.put(protect, admin, updateUser);
 
 module.exports = router;
